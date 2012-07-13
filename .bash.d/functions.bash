7708ecb7e32f0842928711b3081deb368d52c3c8
Thomas Frössman <thomasf@jossystem.se>
2012-07-13 14:59:36 +0200

    Alias d for git diff

--- .bash.d/functions.bash
+++ .bash.d/functions.bash
@@ -85,13 +85,23 @@ du_dir () {
     du -sk ./* | sort -n | awk 'BEGIN{ pref[1]="K"; pref[2]="M"; pref[3]="G";} { total = total + $1; x = $1; y = 1; while( x > 1024 ) { x = (x + 1023)/1024; y++; } printf("%g%s\t%s\n",int(x*10)/10,pref[y],$2); } END { y = 1; while( total > 1024 ) { total = (total + 1023)/1024; y++; } printf("Total: %g%s\n",int(total*10)/10,pref[y]); }'
 }
 
-# Change directory to git repository root
-cdg () {
+
+# Git or possibly other repositories or possibly anything.
+
+# cd to repository root
+cdr () {
     local dir=$(git rev-parse --show-toplevel) && builtin cd $dir
 }
 
-# Git status
-alias s='git status --short'
+s () {
+    git status --short
+}
+
+d () {
+    git diff
+}
+
+
 octopress-create () {
     local name="${1}"
     mkdir $name &&
