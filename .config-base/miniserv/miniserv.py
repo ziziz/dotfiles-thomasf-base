582eb2dbc0d581bc92eb1eb3379da9a8bbd14854
Thomas Frössman <thomasf@jossystem.se>
2013-01-31 20:55:29 +0100

    miniserv

---
+++ .config-base/miniserv/miniserv.py
@@ -0,0 +1,45 @@
+from bottle import route, run, template, static_file, get, post, request
+from subprocess import Popen
+import os
+import sys
+from urllib import quote_plus
+
+root_dir = os.path.dirname(os.path.realpath(__file__))
+files_dir = os.path.join(root_dir, "files")
+port = 7345
+
+
+@route('/')
+def index(name='Hello hello'):
+    return template('<b>Hello {{name}}</b>!', name=name)
+
+
+@get('/files/<filepath:path>')
+def server_static(filepath):
+    return static_file(filepath, root=files_dir)
+
+
+@post('/files/<filepath:path>')
+def update_file(filepath):
+    pass
+
+
+@post('/url')
+def store_url():
+    data = request.json
+    Popen(["emacsclient", "-n", "org-protocol://capture://u/" + quote_plus(data['url'].encode('utf8')) + "/" + quote_plus(data['title'].encode('utf8')) + "/" + quote_plus(data['body'].encode('utf8'))])
+
+
+@get('/org-protocol/<arg:path>')
+def org_protocol(arg):
+    try:
+        Popen(["emacsclient", "-n", arg])
+        return {"ok": True}
+    except Exception:
+        return {"ok": False}
+
+if __name__ == '__main__':
+    if "-d" in sys.argv:
+        run(host='localhost', port=port, quiet=True)
+    else:
+        run(host='localhost', port=port, reloader=True)
