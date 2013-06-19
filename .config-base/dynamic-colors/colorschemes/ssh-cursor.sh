a9f849f762789e78165e23b6a72be0cfd6319225
Thomas Frössman <thomasf@jossystem.se>
2013-06-19 13:54:49 +0200

    ssh cursor color = green

---
+++ .config-base/dynamic-colors/colorschemes/ssh-cursor.sh
@@ -0,0 +1,8 @@
+# Solarized colorscheme "light"
+#
+# See the Solarized homepage for details:
+# http://ethanschoonover.com/solarized
+#
+green="#859900"  # green     #859900  2/2 green     60 -20  65
+
+cursor="$green"
