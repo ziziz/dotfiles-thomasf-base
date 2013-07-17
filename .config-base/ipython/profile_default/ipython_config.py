60ae3f6cde624e3b5de4c6a59d38ae36c60226a0
Thomas Frössman <thomasf@jossystem.se>
2013-07-17 11:28:45 +0200

    add logging to default ipython profile

--- .config-base/ipython/profile_default/ipython_config.py
+++ .config-base/ipython/profile_default/ipython_config.py
@@ -9,3 +9,13 @@ c.InteractiveShellApp.extensions = [
     'importfilemagic',
     'djangomagic',
 ]
+
+# Logging
+import os
+import time
+
+logdir = os.path.expanduser(time.strftime("~/notes/history/ipython/%Y/%m/"))
+logfile = os.path.join(logdir, time.strftime("%d.log"))
+if not os.path.exists(logdir):
+    os.makedirs(logdir)
+c.InteractiveShellApp.exec_lines = ["%%logstart %s append" % logfile]
